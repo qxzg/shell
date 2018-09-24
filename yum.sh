dd4b07b6a9b4b93e9829210416ec6ee13a303712
qxzg <2050545641@qq.com>
2018-09-25 00:48:43 +0800

    一键升级yum源并安装常用软件

--- yum.sh
+++ yum.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-yum clean all
-yum -y update
-yum -y install zip unzip screen net-tools bind-utils vim* make gcc gcc-c++ gdb mtr wget curl automake autoconf time perl-Time-HiRes python perl virt-what gdbautomake ntp gnupg
-yum clean all
+if [ `whoami` = "root" ];then
+	echo "开始安装！"
+	yum clean all#先清空yum缓存
+	yum -y update#升级yum源
+	yum -y install zip unzip screen net-tools bind-utils vim* make gcc gcc-c++ gdb mtr wget curl automake autoconf time perl-Time-HiRes python perl virt-what gdbautomake ntp gnupg#安装软件
+	yum clean all
+	echo "Down!"
+else
+	echo "请以账号root运行这个脚本！"
+	exit
+fi
