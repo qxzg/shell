b33c252d7248d965a4ff94b5f406877afcd1a83b
qxzg <2050545641@qq.com>
2018-09-25 00:27:09 +0800

    一键升级yum源并安装常用软件

--- yum.sh
+++ yum.sh
@@ -2,3 +2,4 @@
 yum clean all
 yum -y update
 yum -y install zip unzip screen net-tools bind-utils vim* make gcc gcc-c++ gdb mtr wget curl automake autoconf time perl-Time-HiRes python perl virt-what gdbautomake ntp gnupg
+yum clean all
