80d497cd12e50ba457ad781073523ca1b6449b9a
qxzg <2050545641@qq.com>
2018-09-25 23:51:31 +0800

    一键配置酸酸乳以及运行环境

--- ssr.sh
+++ ssr.sh
@@ -8,4 +8,7 @@ git clone https://github.com/ssrpanel/shadowsocksr.git /root/ssr
 chmod +x /root/ssr/*.sh
 /root/ssr/setup_cymysql2.sh
 rm -f REL_0_9_4.tar.gz
-echo "Down."
+chmod +x /etc/rc.d/rc.local
+echo "/root/ssr/logrun.sh" >> /etc/rc.d/rc.local
+echo "成功添加开机自启动"
+echo "请编辑/root/ssr/usermysql.json内的数据库连接信息"
