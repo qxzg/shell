017ae6198d7edad5a8a66cd0788bc94cda3021d5
qxzg <2050545641@qq.com>
2018-09-25 21:28:55 +0800

    一键升级yum源并安装常用包

--- yum.sh
+++ yum.sh
@@ -13,7 +13,7 @@ sleep 1
 yum clean all
 yum -y update
 yum -y install epel-release
-yum -y install zip unzip screen net-tool mtr sbind-utils vim* make wget curl time ntp gnupg rsync
+yum -y install zip unzip screen net-tool mtr sbind-utils vim* make wget curl time ntp gnupg rsync xorg-x11-xauth
 if [ ${full} = "1" ];then
 	yum -y install gcc-c++ gdb automake autoconf perl-Time-HiRes python perl virt-what libmcrypt-devel pv pcre-devel openssl openssl-devel gd bzip2 bzip2-devel libevent-devel libxslt-devel freetype-devel unixODBC-devel aspell-devel readline-devel net-snmp-devel enchant-devel gmp-devel
 fi
