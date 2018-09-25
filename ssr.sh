53e8a4e7d0a415e6f7a19621c3c0e3b849c77c0b
qxzg <2050545641@qq.com>
2018-09-25 23:42:27 +0800

    一键配置酸酸乳以及运行环境

--- ssr.sh
+++ ssr.sh
@@ -1,1 +1,11 @@
 #/bin/bash
+yum -y install ntp git wget unzip
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+date -s
+hwclock -w
+ntpdate cn.pool.ntp.org
+git clone https://github.com/ssrpanel/shadowsocksr.git /root/ssr
+chmod +x /root/ssr/*.sh
+/root/ssr/setup_cymysql2.sh
+rm -f REL_0_9_4.tar.gz
+echo "Down."
