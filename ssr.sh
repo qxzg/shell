f0263cdc237bd7ae559c71046b6a022bfac4df46
qxzg <2050545641@qq.com>
2018-10-05 23:25:20 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -4,13 +4,15 @@ cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 date -s
 hwclock -w
 ntpdate cn.pool.ntp.org
+cd
+wget -N --no-check-certificate "https://raw.githubusercontent.com/dlxg/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh
 git clone https://github.com/ssrpanel/shadowsocksr.git /root/ssr
-chmod +x /root/ssr/*.sh
-#/root/ssr/setup_cymysql2.sh
-touch /root/ssr/dns.conf
+cd /root/ssr
+chmod +x *.sh
+./setup_cymysql2.sh
+touch dns.conf
 echo "1.1.1.1 53
-1.0.0.1 53" >> /root/ssr/dns.conf
+1.0.0.1 53" >> dns.conf
 chmod +x /etc/rc.d/rc.local
 echo "/root/ssr/logrun.sh" >> /etc/rc.d/rc.local
-wget -N --no-check-certificate "https://raw.githubusercontent.com/dlxg/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh
-echo "请编辑/root/ssr/usermysql.json内的数据库连接信息，并手动执行/root/ssr/setup_cymysql2.sh"
+echo "请编辑/root/ssr/usermysql.json内的数据库连接信息"
