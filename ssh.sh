e27bc58a1e6c5cceea3a80b93c1a5d132a9aae8c
qxzg <out@qxzg.xyz>
2018-11-04 23:03:28 +0800

    Update ssh.sh

--- ssh.sh
+++ ssh.sh
@@ -8,6 +8,7 @@ chmod 600 authorized_keys
 cd && sed -i -e "s/#PubkeyAuthentication yes/PubkeyAuthentication yes/g" /etc/ssh/sshd_config
 cd && sed -i -e "s/PasswordAuthentication yes/PasswordAuthentication no/g" /etc/ssh/sshd_config
 cd && sed -i -e "s/#Port/Port/g" /etc/ssh/sshd_config
+cd && sed -i -e "s/Port 22/Port 10086/g" /etc/ssh/sshd_config
 ##关闭SELlinux
 setenforce 0
 cd && sed -i -e "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
