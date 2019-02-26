1b28876934916151cde7473d2191be448301a265
qxzg <out@qxzg.xyz>
2019-02-26 15:56:10 +0800

    增加java jdk安装

--- yum.sh
+++ yum.sh
@@ -19,7 +19,7 @@ yum -y update
 yum -y install epel-release
 yum -y install mailcap zip unzip yum-utils screen net-tools mtr bind-utils vim* make wget curl time ntp gnupg rsync xorg-x11-xauth git rng-tools mailcap
 if [ ${full} = "1" ];then
-	yum -y install gcc-c++ gdb automake m2crypto autoconf libsodium perl-Time-HiRes python perl virt-what libmcrypt-devel pv pcre-devel openssl openssl-devel gd bzip2 bzip2-devel libevent-devel libxslt-devel freetype-devel unixODBC-devel aspell-devel readline-devel net-snmp-devel enchant-devel gmp-devel kernel-devel perl-Module-Load-Conditional perl-core check ImageMagick
+	yum -y install gcc-c++ gdb automake m2crypto autoconf libsodium perl-Time-HiRes python perl virt-what libmcrypt-devel pv pcre-devel openssl openssl-devel gd bzip2 bzip2-devel libevent-devel libxslt-devel freetype-devel unixODBC-devel aspell-devel readline-devel net-snmp-devel enchant-devel gmp-devel kernel-devel perl-Module-Load-Conditional perl-core check ImageMagick java-1.8.0-openjdk java-1.8.0-openjdk-devel
 	yum -y groupinstall "Development Tools"
 fi
 echo "Down!"
